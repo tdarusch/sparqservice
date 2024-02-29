f04f912487f5e5b42c043ad3b78f645d3b0ce784
Tom Rusch <trusch@augusta.edu>
2024-02-28 22:43:01 -0500

    fixed warnings, upgraded spring

--- src/main/java/com/sparq/sparqservice/Services/ProfileService.java
+++ src/main/java/com/sparq/sparqservice/Services/ProfileService.java
@@ -1,7 +1,6 @@
 package com.sparq.sparqservice.Services;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
